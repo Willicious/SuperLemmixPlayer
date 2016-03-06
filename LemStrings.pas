10e4efff2b3a9fb9d79d0bfe4e836b3343f52491
namida <hayanninja@yandex.com>
2016-03-06 18:36:07 +1300

    Set version numbers, removed experimental marker

--- LemStrings.pas
+++ LemStrings.pas
@@ -16,12 +16,12 @@ uses
   function NumericalVersionToStringVersion(Main, Sub, Minor: Integer): String;
 
 const
-  PVersion = '1.43n-C';
+  PVersion = '1.43n-D';
 
   // Current version number
   Cur_MainVer = 1;
   Cur_SubVer = 43;
-  Cur_MinorVer = 3;
+  Cur_MinorVer = 4;
 
   // Do not forget to update consts in TAppController.CheckCompatibility if minimum compatible version has changed!
 
