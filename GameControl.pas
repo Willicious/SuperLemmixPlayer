8047d351bd0347dff4ba15988d3ac8d345a67b1c
Namida <hayanninja@yandex.com>
2016-03-16 02:50:13 +1300

    Genericized the option setter / getter functions

--- GameControl.pas
+++ GameControl.pas
@@ -103,7 +103,8 @@ type
     moIgnoreReplaySelection,
     moEnableOnline,
     moCheckUpdates,
-    mo30, mo31
+    moNoAutoReplayMode,
+    mo31
   );
 
   TMiscOptions = set of TMiscOption;
@@ -133,7 +134,13 @@ type
     fZoomLevel: Integer;
     fMainForm: TForm; // link to the FMain form
 
-    function GetCheatCodesEnabled: Boolean;
+    function GetOptionFlag(aFlag: TMiscOption): Boolean;
+    procedure SetOptionFlag(aFlag: TMiscOption; aValue: Boolean);
+
+    function GetSoundFlag(aFlag: TGameSoundOption): Boolean;
+    procedure SetSoundFlag(aFlag: TGameSoundOption; aValue: Boolean);
+
+    {function GetCheatCodesEnabled: Boolean;
     procedure SetCheatCodesEnabled(Value: Boolean);
     function GetMusicEnabled: Boolean;
     function GetSoundEnabled: Boolean;
@@ -170,7 +177,7 @@ type
     function GetEnableOnline: Boolean;
     procedure SetEnableOnline(Value: Boolean);
     function GetCheckUpdates: Boolean;
-    procedure SetCheckUpdates(Value: Boolean);
+    procedure SetCheckUpdates(Value: Boolean);}
   public
     // this is initialized by appcontroller
     MainDatFile  : string;
@@ -234,34 +241,36 @@ type
     procedure LoadFromIniFile;
     procedure SaveToIniFile;
     property SaveSystem: TNeoSave read fSaveSystem;
-    property CheatCodesEnabled: Boolean read GetCheatCodesEnabled write SetCheatCodesEnabled;
-    property LookForLVLFiles: Boolean read GetLookForLVLFiles write SetLookForLVLFiles;
-    property DebugSteel: Boolean read GetDebugSteel write SetDebugSteel;
-    property ChallengeMode: Boolean read GetChallengeMode write SetChallengeMode;
-    property TimerMode: Boolean read GetTimerMode write SetTimerMode;
-    property ForceSkillset: Word read fForceSkillset write fForceSkillset;
-    property Directory: string read fDirectory write fDirectory;
-    property DumpMode: boolean read fDumpMode write fDumpMode;
-    property QuickTestMode: Integer read fTestScreens write fTestScreens;
-    property MusicEnabled: Boolean read GetMusicEnabled write SetMusicEnabled;
-    property SoundEnabled: Boolean read GetSoundEnabled write SetSoundEnabled;
-    property ClickHighlight: Boolean read GetClickHighlight write SetClickHighlight;
-    property AutoReplayNames: Boolean read GetAutoReplayNames write SetAutoReplayNames;
-    property AutoSaveReplay: Boolean read GetAutoReplaySave write SetAutoReplaySave;
-    property LemmingBlink: Boolean read GetLemmingBlink write SetLemmingBlink;
-    property TimerBlink: Boolean read GetTimerBlink write SetTimerBlink;
-    property ShownText: boolean read fShownText write fShownText;
-    property OneLevelMode: boolean read fOneLevelMode write fOneLevelMode;
-    property AlwaysTimestamp: boolean read GetAlwaysTimestamp write SetAlwaysTimestamp;
-    property ConfirmOverwrite: boolean read GetConfirmOverwrite write SetConfirmOverwrite;
-    property ExplicitCancel: boolean read GetExplicitCancel write SetExplicitCancel;
-    property WhiteOutZero: boolean read GetWhiteOutZero write SetWhiteOutZero;
-    property IgnoreReplaySelection: boolean read GetIgnoreReplaySelection write SetIgnoreReplaySelection;
-    property EnableOnline: boolean read GetEnableOnline write SetEnableOnline;
-    property CheckUpdates: boolean read GetCheckUpdates write SetCheckUpdates;
 
+    property MusicEnabled: Boolean Index gsoMusic read GetSoundFlag write SetSoundFlag;
+    property SoundEnabled: Boolean Index gsoSound read GetSoundFlag write SetSoundFlag;
+
+    property CheatCodesEnabled: Boolean Index moCheatCodes read GetOptionFlag write SetOptionFlag;
+    property LookForLVLFiles: Boolean Index moLookForLVLFiles read GetOptionFlag write SetOptionFlag;
+    property DebugSteel: Boolean Index moDebugSteel read GetOptionFlag write SetOptionFlag;
+    property ChallengeMode: Boolean Index moChallengeMode read GetOptionFlag write SetOptionFlag;
+    property TimerMode: Boolean Index moTimerMode read GetOptionFlag write SetOptionFlag;
+    property ClickHighlight: Boolean Index moClickHighlight read GetOptionFlag write SetOptionFlag;
+    property AutoReplayNames: Boolean Index moAutoReplayNames read GetOptionFlag write SetOptionFlag;
+    property AutoSaveReplay: Boolean Index moAutoReplaySave read GetOptionFlag write SetOptionFlag;
+    property LemmingBlink: Boolean Index moLemmingBlink read GetOptionFlag write SetOptionFlag;
+    property TimerBlink: Boolean Index moTimerBlink read GetOptionFlag write SetOptionFlag;
+    property AlwaysTimestamp: boolean Index moAlwaysTimestamp read GetOptionFlag write SetOptionFlag;
+    property ConfirmOverwrite: boolean Index moConfirmOverwrite read GetOptionFlag write SetOptionFlag;
+    property ExplicitCancel: boolean Index moExplicitCancel read GetOptionFlag write SetOptionFlag;
+    property WhiteOutZero: boolean Index moWhiteOutZero read GetOptionFlag write SetOptionFlag;
+    property IgnoreReplaySelection: boolean Index moIgnoreReplaySelection read GetOptionFlag write SetOptionFlag;
+    property EnableOnline: boolean Index moEnableOnline read GetOptionFlag write SetOptionFlag;
+    property CheckUpdates: boolean Index moCheckUpdates read GetOptionFlag write SetOptionFlag;
+
+    property DumpMode: boolean read fDumpMode write fDumpMode;
+    property OneLevelMode: boolean read fOneLevelMode write fOneLevelMode;
+    property ShownText: boolean read fShownText write fShownText;
     property DoneUpdateCheck: Boolean read fDoneUpdateCheck write fDoneUpdateCheck;
 
+    property Directory: string read fDirectory write fDirectory;
+    property ForceSkillset: Word read fForceSkillset write fForceSkillset;
+    property QuickTestMode: Integer read fTestScreens write fTestScreens;
     property ZoomLevel: Integer read fZoomLevel write fZoomLevel;
     {property WindowX: Integer read fWindowX write fWindowX;
     property WindowY: Integer read fWindowY write fWindowY;}
@@ -418,251 +427,30 @@ begin
   inherited Destroy;
 end;
 
-function TDosGameParams.GetCheatCodesEnabled: Boolean;
+function TDosGameParams.GetOptionFlag(aFlag: TMiscOption): Boolean;
 begin
-  Result := moCheatCodes in MiscOptions;
+  Result := aFlag in MiscOptions;
 end;
 
-function TDosGameParams.GetMusicEnabled: Boolean;
+procedure TDosGameParams.SetOptionFlag(aFlag: TMiscOption; aValue: Boolean);
 begin
-  Result := gsoMusic in SoundOptions;
+  if aValue then
+    Include(MiscOptions, aFlag)
+  else
+    Exclude(MiscOptions, aFlag);
 end;
 
-function TDosGameParams.GetSoundEnabled: Boolean;
+function TDosGameParams.GetSoundFlag(aFlag: TGameSoundOption): Boolean;
 begin
-  Result := gsoSound in SoundOptions;
+  Result := aFlag in SoundOptions;
 end;
 
-function TDosGameParams.GetLookForLVLFiles: Boolean;
+procedure TDosGameParams.SetSoundFlag(aFlag: TGameSoundOption; aValue: Boolean);
 begin
-  Result := moLookForLVLFiles in MiscOptions;
-end;
-
-function TDosGameParams.GetDebugSteel: Boolean;
-begin
-  Result := moDebugSteel in MiscOptions;
-end;
-
-function TDosGameParams.GetChallengeMode: Boolean;
-begin
-  Result := moChallengeMode in MiscOptions;
-end;
-
-function TDosGameParams.GetTimerMode: Boolean;
-begin
-  Result := moTimerMode in MiscOptions;
-end;
-
-function TDosGameParams.GetAutoReplayNames: Boolean;
-begin
-  Result := moAutoReplayNames in MiscOptions;
-end;
-
-function TDosGameParams.GetAutoReplaySave: Boolean;
-begin
-  Result := moAutoReplaySave in MiscOptions;
-end;
-
-function TDosGameParams.GetLemmingBlink: Boolean;
-begin
-  Result := moLemmingBlink in MiscOptions;
-end;
-
-function TDosGameParams.GetTimerBlink: Boolean;
-begin
-  Result := moTimerBlink in MiscOptions;
-end;
-
-function TDosGameParams.GetClickHighlight: Boolean;
-begin
-  Result := moClickHighlight in MiscOptions;
-end;
-
-function TDosGameParams.GetAlwaysTimestamp: Boolean;
-begin
-  Result := moAlwaysTimestamp in MiscOptions;
-end;
-
-function TDosGameParams.GetConfirmOverwrite: Boolean;
-begin
-  Result := moConfirmOverwrite in MiscOptions;
-end;
-
-function TDosGameParams.GetExplicitCancel: Boolean;
-begin
-  Result := moExplicitCancel in MiscOptions;
-end;
-
-procedure TDosGameParams.SetLemmingBlink(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moLemmingBlink);
-    True:  Include(MiscOptions, moLemmingBlink);
-  end;
-end;
-
-procedure TDosGameParams.SetTimerBlink(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moTimerBlink);
-    True:  Include(MiscOptions, moTimerBlink);
-  end;
-end;
-
-procedure TDosGameParams.SetAutoReplayNames(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moAutoReplayNames);
-    True:  Include(MiscOptions, moAutoReplayNames);
-  end;
-end;
-
-procedure TDosGameParams.SetAutoReplaySave(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moAutoReplaySave);
-    True:  Include(MiscOptions, moAutoReplaySave);
-  end;
-end;
-
-procedure TDosGameParams.SetCheatCodesEnabled(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moCheatCodes);
-    True:  Include(MiscOptions, moCheatCodes);
-  end;
-end;
-
-procedure TDosGameParams.SetMusicEnabled(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(SoundOptions, gsoMusic);
-    True:  Include(SoundOptions, gsoMusic);
-  end;
-end;
-
-procedure TDosGameParams.SetSoundEnabled(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(SoundOptions, gsoSound);
-    True:  Include(SoundOptions, gsoSound);
-  end;
-end;
-
-procedure TDosGameParams.SetLookForLVLFiles(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moLookForLVLFiles);
-    True:  Include(MiscOptions, moLookForLVLFiles);
-  end;
-end;
-
-procedure TDosGameParams.SetDebugSteel(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moDebugSteel);
-    True:  Include(MiscOptions, moDebugSteel);
-  end;
-end;
-
-procedure TDosGameParams.SetTimerMode(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moTimerMode);
-    True:  Include(MiscOptions, moTimerMode);
-  end;
-end;
-
-procedure TDosGameParams.SetChallengeMode(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moChallengeMode);
-    True:  Include(MiscOptions, moChallengeMode);
-  end;
-end;
-
-procedure TDosGameParams.SetClickHighlight(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moClickHighlight);
-    True:  Include(MiscOptions, moClickHighlight);
-  end;
-end;
-
-procedure TDosGameParams.SetAlwaysTimestamp(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moAlwaysTimestamp);
-    True:  Include(MiscOptions, moAlwaysTimestamp);
-  end;
-end;
-
-procedure TDosGameParams.SetConfirmOverwrite(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moConfirmOverwrite);
-    True:  Include(MiscOptions, moConfirmOverwrite);
-  end;
-end;
-
-procedure TDosGameParams.SetExplicitCancel(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moExplicitCancel);
-    True:  Include(MiscOptions, moExplicitCancel);
-  end;
-end;
-
-function TDosGameParams.GetWhiteOutZero: Boolean;
-begin
-  Result := moWhiteOutZero in MiscOptions;
-end;
-
-procedure TDosGameParams.SetWhiteOutZero(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moWhiteOutZero);
-    True:  Include(MiscOptions, moWhiteOutZero);
-  end;
-end;
-
-function TDosGameParams.GetIgnoreReplaySelection: Boolean;
-begin
-  Result := moIgnoreReplaySelection in MiscOptions;
-end;
-
-procedure TDosGameParams.SetIgnoreReplaySelection(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moIgnoreReplaySelection);
-    True:  Include(MiscOptions, moIgnoreReplaySelection);
-  end;
-end;
-
-function TDosGameParams.GetEnableOnline: Boolean;
-begin
-  Result := moEnableOnline in MiscOptions;
-end;
-
-procedure TDosGameParams.SetEnableOnline(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moEnableOnline);
-    True:  Include(MiscOptions, moEnableOnline);
-  end;
-end;
-
-function TDosGameParams.GetCheckUpdates: Boolean;
-begin
-  Result := moCheckUpdates in MiscOptions;
-end;
-
-procedure TDosGameParams.SetCheckUpdates(Value: Boolean);
-begin
-  case Value of
-    False: Exclude(MiscOptions, moCheckUpdates);
-    True:  Include(MiscOptions, moCheckUpdates);
-  end;
+  if aValue then
+    Include(SoundOptions, aFlag)
+  else
+    Exclude(SoundOptions, aFlag);
 end;
 
 end.
