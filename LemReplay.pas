762f3407195d2fc001bc38ba44a0a336e250d701
Namida <hayanninja@yandex.com>
2016-06-30 09:06:27 +1200

    New replay should theoretically save to a file now. I say theoretically, because it can't be tested until LemGame actually /uses/ it.

--- LemReplay.pas
+++ LemReplay.pas
@@ -16,8 +16,18 @@ unit LemReplay;
 interface
 
 uses
-  LemNeoParser, LemLemming, LemCore,
+  LemNeoParser, LemLemming, LemCore, LemStrings,
   Contnrs, Classes, SysUtils;
+
+const
+  SKILL_REPLAY_NAME_COUNT = 16;
+  SKILL_REPLAY_NAMES: array[0..SKILL_REPLAY_NAME_COUNT-1] of String =
+                                               ('WALKER', 'CLIMBER', 'SWIMMER',
+                                                'FLOATER', 'GLIDER', 'DISARMER',
+                                                'BOMBER', 'STONER', 'BLOCKER',
+                                                'PLATFORMER', 'BUILDER', 'STACKER',
+                                                'BASHER', 'MINER', 'DIGGER',
+                                                'CLONER');
 
 type
   TReplayAction = (ra_None, ra_AssignSkill, ra_ChangeReleaseRate, ra_Nuke,
@@ -68,7 +78,7 @@ type
     private
       fSkill: TSkillPanelButton;
     public
-      property Skill: TSkillPanelButton read fSkill;
+      property Skill: TSkillPanelButton read fSkill write fSkill;
   end;
 
   TReplayHighlightLemming = class(TBaseReplayLemmingItem)
@@ -90,16 +100,70 @@ type
       fAssignments: TReplayItemList;        // nuking is also included here
       fReleaseRateChanges: TReplayItemList;
       fInterfaceActions: TReplayItemList;
-      fLevelID: Integer;
+      fPlayerName: String;
+      fLevelName: String;
+      fLevelAuthor: String;
+      fLevelGame: String;
+      fLevelRank: String;
+      fLevelPosition: Integer;
+      fLevelID: Cardinal;
+      function GetItemByFrame(aItemType: Integer; aFrame: Integer): TBaseReplayItem;
+      procedure SaveReplayList(aList: TReplayItemList; SL: TStringList);
+      procedure SaveReplayItem(aItem: TBaseReplayItem; SL: TStringList);
     public
       constructor Create;
       destructor Destroy; override;
       procedure Clear;
+      procedure LoadFromFile(aFile: String);
+      procedure SaveToFile(aFile: String);
       procedure LoadOldReplayFile(aFile: String);
-      property LevelID: Integer read fLevelID write fLevelID;
+      property LevelName: String read fLevelName write fLevelName;
+      property LevelAuthor: String read fLevelAuthor write fLevelAuthor;
+      property LevelGame: String read fLevelReference write fLevelReference;
+      property LevelRank: String read fLevelRank write fLevelRank;
+      property LevelPosition: String read fLevelPosition write fLevelPosition;
+      property LevelID: Cardinal read fLevelID write fLevelID;
+      property Assignment[aFrame: Integer]: TBaseReplayItem Index 1 read GetItemByFrame;
+      property ReleaseRateChange[aFrame: Integer]: TBaseReplayItem Index 2 read GetItemByFrame;
+      property InterfaceAction[aFrame: Integer]: TBaseReplayItem Index 3 read GetItemByFrame;
   end;
 
+  function GetSkillReplayName(aButton: TSkillPanelButton): String; overload;
+  function GetSkillReplayName(aAction: TBasicLemmingAction): String; overload;
+  function GetSkillButton(aName: String): TSkillPanelButton;
+  function GetSkillAction(aName: String): TBasicLemmingAction;
+
 implementation
+
+// Standalone functions
+
+function GetSkillReplayName(aButton: TSkillPanelButton): String;
+begin
+  Result := SKILL_REPLAY_NAMES[Integer(aButton)];
+end;
+
+function GetSkillReplayName(aAction: TBasicLemmingAction): String;
+begin
+  Result := GetSkillReplayName(ActionToSkillPanelButton[aAction]);
+end;
+
+function GetSkillButton(aName: String): TSkillPanelButton;
+var
+  i: Integer;
+begin
+  aName := Uppercase(aName);
+  for i := 0 to SKILL_REPLAY_NAME_COUNT-1 do
+    if aName = SKILL_REPLAY_NAMES[i] then
+    begin
+      Result := TSkillPanelButton(i);
+      Exit;
+    end;
+end;
+
+function GetSkillAction(aName: String): TBasicLemmingAction;
+begin
+  Result := SkillPanelButtonToAction[GetSkillButton(aName)];
+end;
 
 // Stuff for the old LRB format
 type
@@ -179,6 +243,111 @@ begin
   fLevelID := 0;
 end;
 
+procedure TReplay.LoadFromFile(aFile: String);
+begin
+end;
+
+procedure TReplay.SaveToFile(aFile: String);
+var
+  SL: TStringList;
+  E: TBaseReplayItem; //why do I keep naming it E? I don't know. But it's become a thing now.
+  i: Integer;
+begin
+  SL := TStringList.Create;
+
+  SL.Add('# NeoLemmix Replay File');
+  SL.Add('# Saved from NeoLemmix V' + PVersion);
+  SL.Add('');
+  if fPlayerName <> '' then
+    SL.Add('USER ' + fPlayerName);
+  SL.Add('TITLE ' + fLevelName);
+  if fLevelAuthor <> '' then
+    SL.Add('AUTHOR ' + fLevelAuthor);
+  if fLevelGame <> '' then
+  begin
+    SL.Add('GAME ' + fLevelGame);
+    SL.Add('RANK ' + fLevelRank);
+    SL.Add('LEVEL ' + IntToStr(fLevelPosition));
+  end;
+  SL.Add('ID ' + IntToHex(fLevelID));
+  SL.Add('');
+
+  SL.SaveToFile(aFile);
+
+  SL.Free;
+end;
+
+procedure TReplay.SaveReplayList(aList: TReplayItemList; SL: TStringList);
+var
+  i: Integer;
+begin
+  for i := 0 to aList.Count-1 do
+    SaveReplayItem(aList[i], SL);
+end;
+
+procedure TReplay.SaveReplayItem(aItem: TBaseReplayItem, SL: TStringList);
+
+  procedure SaveLemmingEntry;
+  var
+    E: TBaseReplayLemmingItem absolute aItem;
+  begin
+    SL.Add('  LEM_INDEX ' + IntToStr(E.LemmingIndex));
+    SL.Add('  LEM_X ' + IntToStr(E.LemmingX));
+    SL.Add('  LEM_Y ' + IntToStr(E.LemmingY));
+    if E.fLemmingDx < 0 then
+      SL.Add('  LEM_DIR LEFT')
+    else if E.fLemmingDx > 0 then
+      SL.Add('  LEM_DIR RIGHT'); //some old replay files, we might not know either way, so it could be neither
+    if E.LemmingHighlit then
+      SL.Add('  HIGHLIT');
+  end;
+
+  procedure SaveAssignEntry;
+  var
+    E: TReplaySkillAssignment absolute aItem;
+  begin
+    SL.Add('ASSIGNMENT');
+    SL.Add('  ACTION ' + GetSkillReplayName(E.Skill));
+    SaveLemmingEntry;
+  end;
+
+  procedure SaveNukeEntry;
+  var
+    E: TReplayNuke absolute aItem;
+  begin
+    SL.Add('NUKE');
+  end;
+
+  procedure SaveReleaseRateEntry;
+  var
+    E: TReplayChangeReleaseRate absolute aItem;
+  begin
+    SL.Add('RELEASE_RATE');
+    SL.Add('  RATE ' + IntToStr(E.NewReleaseRate));
+    SL.Add('  SPAWNED ' + IntToStr(E.SpawnedLemmingCount));
+  end;
+
+  procedure SaveSelectSkillEntry;
+  var
+    E: TReplaySelectSkill absolute aItem;
+  begin
+    SL.Add('SELECT_SKILL');
+    SL.Add('  SKILL ' + GetSkillReplayName(E.Skill));
+  end;
+
+  procedure SaveHighlightEntry;
+  var
+    E: TReplayHighlightLemming absolute aItem;
+  begin
+    SL.Add('HIGHLIGHT');
+    SaveLemmingEntry;
+  end;
+begin
+
+  SL.Add('FRAME ' + IntToStr(aItem.Frame));
+  SL.Add('');
+end;
+
 procedure TReplay.LoadOldReplayFile(aFile: String);
 var
   MS: TMemoryStream;
@@ -192,7 +361,7 @@ var
     E: TReplaySkillAssignment;
   begin
     E := TReplaySkillAssignment.Create;
-    //E.Skill := // Need to make a conversion function
+    E.Skill := TBasicLemmingAction(Item.AssignedSkill);
     E.LemmingIndex := Item.LemmingIndex;
     E.LemmingX := Item.LemmingX;
     E.LemmingDx := Item.SelectDir; // it's the closest we've got
@@ -226,7 +395,7 @@ var
     E: TReplaySelectSkill;
   begin
     E := TReplaySelectSkill.Create;
-    //E.Skill := //need to do
+    E.Skill := TSkillPanelButton(Item.SelectedButton);
     E.Frame := Item.Iteration;
     fInterfaceActions.Add(E);
   end;
@@ -240,14 +409,53 @@ begin
     MS.Read(Header, SizeOf(TReplayFileHeaderRec));
 
     MS.Position := Header.FirstRecordPos;
+    LastReleaseRate := 0;
+    LastSelectedSkill := spbNone;
 
     while MS.Read(Item, SizeOf(TReplayRec)) = SizeOf(TReplayRec) do
     begin
-    
+      if Item.ReleaseRate <> LastReleaseRate then
+      begin
+        CreateReleaseRateEntry;
+        LastReleaseRate := Item.ReleaseRate;
+      end;
+
+      if TSkillPanelButton(Item.SelectedButton) <> LastSelectedSkill then
+      begin
+        CreateSelectSkillEntry;
+        LastSelectedSkill := TSkillPanelButton(Item.SelectedButton);
+      end;
+
+      if Item.ActionFlags and raf_SkillAssignment <> 0 then
+        CreateAssignEntry;
+
+      if Item.ActionFlags and raf_Nuke <> 0 then
+        CreateNukeEntry;
     end;
   finally
     MS.Free;
   end;
+end;
+
+function TReplay.GetItemByFrame(aItemType: Integer; aFrame: Integer): TBaseReplayItem;
+var
+  i: Integer;
+  L: TReplayItemList;
+begin
+  Result := nil;
+  case aItemType of
+    1: L := fAssignments;
+    2: L := fReleaseRateChanges;
+    3: L := fInterfaceActions;
+    else Exit;
+  end;
+
+  for i := 0 to L.Count-1 do
+    if L[i].Frame = aFrame then
+    begin
+      Result := L[i];
+      Exit;
+    end;
 end;
 
 { TReplayItemList }
