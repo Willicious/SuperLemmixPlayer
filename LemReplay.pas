94abb7c97c16b49b6ae67d105a76fa197e77e017
Namida <hayanninja@yandex.com>
2016-06-30 05:46:49 +1200

    Some incomplete code towards importing old-format replays.

--- LemReplay.pas
+++ LemReplay.pas
@@ -58,6 +58,7 @@ type
       fSpawnedLemmingCount: Integer;
     public
       property NewReleaseRate: Integer read fNewReleaseRate write fNewReleaseRate;
+      property SpawnedLemmingCount: Integer read fSpawnedLemmingCount write fSpawnedLemmingCount;
   end;
 
   TReplayNuke = class(TBaseReplayItem)
@@ -78,8 +79,8 @@ type
       function GetItem(Index: Integer): TBaseReplayItem;
     public
       constructor Create;
-      function Add: TBaseReplayItem;
-      function Insert(Index: Integer): TBaseReplayItem;
+      function Add(Item: TBaseReplayItem): Integer;
+      procedure Insert(Index: Integer; Item: TBaseReplayItem);
       property Items[Index: Integer]: TBaseReplayItem read GetItem; default;
       property List;
   end;
@@ -89,14 +90,67 @@ type
       fAssignments: TReplayItemList;        // nuking is also included here
       fReleaseRateChanges: TReplayItemList;
       fInterfaceActions: TReplayItemList;
+      fLevelID: Integer;
     public
       constructor Create;
       destructor Destroy; override;
       procedure Clear;
       procedure LoadOldReplayFile(aFile: String);
+      property LevelID: Integer read fLevelID write fLevelID;
   end;
 
 implementation
+
+// Stuff for the old LRB format
+type
+  TReplayFileHeaderRec = packed record
+    Signature         : array[0..2] of Char;     //  3 bytes -  3
+    Version           : Byte;                    //  1 byte  -  4
+    FileSize          : Integer;                 //  4 bytes -  8
+    HeaderSize        : Word;                    //  2 bytes - 10
+    Mechanics         : Word;                    //  2 bytes - 12
+    FirstRecordPos    : Integer;                 //  4 bytes - 16
+    ReplayRecordSize  : Word;                    //  2 bytes - 18
+    ReplayRecordCount : Word;                    //  2 bytes - 20
+
+    ReplayGame        : Byte;
+    ReplaySec         : Byte;
+    ReplayLev         : Byte;
+    ReplayOpt         : Word;
+
+    ReplayTime        : LongWord;
+    ReplaySaved       : Word;
+
+    ReplayLevelID    : LongWord;
+
+    Reserved        : array[0..29] of Char;
+  end;
+
+  TReplayRec = packed record
+    Check          : Char;         //  1 byte  -  1
+    Iteration      : Integer;      //  4 bytes -  5
+    ActionFlags    : Word;         //  2 bytes -  7
+    AssignedSkill  : Byte;         //  1 byte  -  8
+    SelectedButton : Byte;         //  1 byte  -  9
+    ReleaseRate    : Integer;      //  1 byte  - 13
+    LemmingIndex   : Integer;      //  4 bytes - 17
+    LemmingX       : Integer;      //  4 bytes - 21
+    LemmingY       : Integer;      //  4 bytes - 25
+    CursorX        : SmallInt;     //  2 bytes - 27
+    CursorY        : SmallInt;     //  2 bytes - 29
+    SelectDir      : ShortInt;
+    Reserved2      : Byte;
+    Reserved3      : Byte;         // 32
+  end;
+
+const
+  //Recorded Action Flags
+	raf_StartIncreaseRR   = $04;
+	raf_StartDecreaseRR   = $08;
+	raf_StopChangingRR    = $10;
+	raf_SkillSelection    = $20;
+	raf_SkillAssignment   = $40;
+	raf_Nuke              = $80;
 
 { TReplay }
 
@@ -106,6 +160,7 @@ begin
   fAssignments := TReplayItemList.Create;
   fReleaseRateChanges := TReplayItemList.Create;
   fInterfaceActions := TReplayItemList.Create;
+  Clear;
 end;
 
 destructor TReplay.Destroy;
@@ -121,16 +176,75 @@ begin
   fAssignments.Clear;
   fReleaseRateChanges.Clear;
   fInterfaceActions.Clear;
+  fLevelID := 0;
 end;
 
 procedure TReplay.LoadOldReplayFile(aFile: String);
 var
   MS: TMemoryStream;
+  Header: TReplayFileHeaderRec;
+  Item: TReplayRec;
+  LastReleaseRate: Integer;
+  LastSelectedSkill: TSkillPanelButton;
+
+  procedure CreateAssignEntry;
+  var
+    E: TReplaySkillAssignment;
+  begin
+    E := TReplaySkillAssignment.Create;
+    //E.Skill := // Need to make a conversion function
+    E.LemmingIndex := Item.LemmingIndex;
+    E.LemmingX := Item.LemmingX;
+    E.LemmingDx := Item.SelectDir; // it's the closest we've got
+    E.LemmingHighlit := false; // we can't tell for old replays
+    E.Frame := Item.Iteration;
+    fAssignments.Add(E);
+  end;
+
+  procedure CreateNukeEntry;
+  var
+    E: TReplayNuke;
+  begin
+    E := TReplayNuke.Create;
+    E.Frame := Item.Iteration;
+    fAssignments.Add(E);
+  end;
+
+  procedure CreateReleaseRateEntry;
+  var
+    E: TReplayChangeReleaseRate;
+  begin
+    E := TReplayChangeReleaseRate.Create;
+    E.NewReleaseRate := Item.ReleaseRate;
+    E.SpawnedLemmingCount := -1; // we don't know
+    E.Frame := Item.Iteration;
+    fReleaseRateChanges.Add(E);
+  end;
+
+  procedure CreateSelectSkillEntry;
+  var
+    E: TReplaySelectSkill;
+  begin
+    E := TReplaySelectSkill.Create;
+    //E.Skill := //need to do
+    E.Frame := Item.Iteration;
+    fInterfaceActions.Add(E);
+  end;
+
 begin
   Clear;
   MS := TMemoryStream.Create;
   try
+    MS.LoadFromFile(aFile);
+    MS.Position := 0;
+    MS.Read(Header, SizeOf(TReplayFileHeaderRec));
 
+    MS.Position := Header.FirstRecordPos;
+
+    while MS.Read(Item, SizeOf(TReplayRec)) = SizeOf(TReplayRec) do
+    begin
+    
+    end;
   finally
     MS.Free;
   end;
@@ -146,16 +260,14 @@ begin
   inherited Create(aOwnsObjects);
 end;
 
-function TReplayItemList.Add: TBaseReplayItem;
+function TReplayItemList.Add(Item: TBaseReplayItem): Integer;
 begin
-  Result := TBaseReplayItem.Create;
-  inherited Add(Result);
+  Result := inherited Add(Item);
 end;
 
-function TReplayItemList.Insert(Index: Integer): TBaseReplayItem;
+procedure TReplayItemList.Insert(Index: Integer; Item: TBaseReplayItem);
 begin
-  Result := TBaseReplayItem.Create;
-  inherited Insert(Index, Result);
+  inherited Insert(Index, Item);
 end;
 
 function TReplayItemList.GetItem(Index: Integer): TBaseReplayItem;
