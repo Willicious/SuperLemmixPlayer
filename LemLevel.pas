aab990f171c68bbae6fdfb9505a773ba8c6f39c2
Namida <hayanninja@yandex.com>
2016-12-05 02:51:45 +1300

    Trimmed some fat from LemLevel.pas. Some of this needs to be restored, but it's going to be tidier to re-do it from scratch. Also, level loading code should be moved into here.

--- LemLevel.pas
+++ LemLevel.pas
@@ -44,43 +44,23 @@ type
     fWidth : Integer;
     fHeight : Integer;
 
-    fGraphicSet     : Integer;
-    fGraphicSetEx   : Integer;
-
     fBackgroundIndex: Integer;
 
     fLevelOptions   : Cardinal;
 
-    fSuperLemming   : Integer;
     fScreenPosition : Integer;
     fScreenYPosition: Integer;
-    fDisplayPercent : Integer;
     fTitle          : string;
     fAuthor         : string;
 
-    fGraphicSetFile : string;
-    fVgaspecFile    : string;
-    fGraphicMetaFile : string;
     fGraphicSetName : string;
     fMusicFile      : string;
-
-    fGimmickSet     : LongWord;
-    fGimmickSet2    : LongWord;
-    fGimmickSet3    : LongWord;
-
-    fVgaspecX       : Integer;
-    fVgaspecY       : Integer;
-
-    fBnsRank        : Integer;
-    fBnsLevel       : Integer;
 
     fLevelID        : LongWord;
   protected
   public
-    fOddtarget        : Integer;
     WindowOrder       : array of word;
     constructor Create;
-    procedure Assign(Source: TPersistent); override;
     procedure Clear; virtual;
   published
     property ReleaseRate    : Integer read fReleaseRate write fReleaseRate;
@@ -109,12 +89,8 @@ type
 
     property SkillTypes     : Integer read fSkillTypes write fSkillTypes;
 
-    property GraphicSet     : Integer read fGraphicSet write fGraphicSet;
-    property GraphicSetEx   : Integer read fGraphicSetEx write fGraphicSetEx;
-    property SuperLemming   : Integer read fSuperLemming write fSuperLemming;
     property ScreenPosition : Integer read fScreenPosition write fScreenPosition;
     property ScreenYPosition : Integer read fScreenYPosition write fScreenYPosition;
-    property DisplayPercent : Integer read fDisplayPercent write fDisplayPercent;
     property Title          : string read fTitle write fTitle;
     property Author         : string read fAuthor write fAuthor;
 
@@ -122,65 +98,31 @@ type
 
     property Width          : Integer read fWidth write fWidth;
     property Height         : Integer read fHeight write fHeight;
-    property VgaspecX       : Integer read fVgaspecX write fVgaspecX;
-    property VgaspecY       : Integer read fVgaspecY write fVgaspecY;
 
-    property GimmickSet     : LongWord read fGimmickSet write fGimmickSet;
-    property GimmickSet2    : LongWord read fGimmickSet2 write fGimmickSet2;
-    property GimmickSet3    : LongWord read fGimmickSet3 write fGimmickSet3;
-
-    property GraphicMetaFile : String read fGraphicMetaFile write fGraphicMetaFile;
-    property GraphicSetFile : String read fGraphicSetFile write fGraphicSetFile;
-    property VgaspecFile    : String read fVgaspecFile write fVgaspecFile;
     property GraphicSetName : String read fGraphicSetName write fGraphicSetName;
     property MusicFile      : String read fMusicFile write fMusicFile;
 
     property BackgroundIndex: Integer read fBackgroundIndex write fBackgroundIndex;
 
-    property BnsRank: Integer read fBnsRank write fBnsRank;
-    property BnsLevel: Integer read fBnsLevel write fBnsLevel;
-
     property LevelID: LongWord read fLevelID write fLevelID;
   end;
 
-  TLevel = class(TComponent)
+  TLevel = class
   private
-  protected
     fLevelInfo       : TLevelInfo;
     fTerrains           : TTerrains;
     fInteractiveObjects : TInteractiveObjects;
     fSteels             : TSteels;
     fPreplacedLemmings  : TPreplacedLemmingList;
-  { internal }
-    fUpdateCounter      : Integer;
-  { property access }
-    procedure SetTerrains(Value: TTerrains); virtual;
-    procedure SetInteractiveObjects(Value: TInteractiveObjects); virtual;
-    procedure SetSteels(Value: TSteels); virtual;
-    procedure SetLevelInfo(const Value: TLevelInfo); virtual;
-  { dynamic creation }
-    function DoCreateLevelInfo: TLevelInfo; dynamic;
-    function DoCreateTerrains: TTerrains; dynamic;
-    function DoCreateInteractiveObjects: TInteractiveObjects; dynamic;
-    function DoCreateSteels: TSteels; dynamic;
-  { protected overrides }
   public
-    { TODO : TLevel maybe does not need to be a Component }
-    constructor Create(aOwner: TComponent); override;
+    constructor Create;
     destructor Destroy; override;
-    procedure Assign(Source: TPersistent); override;
-    procedure AssignOddtable(Source: TLevel);
-    procedure SaveToFile(const aFileName: string);
-    procedure SaveToStream(S: TStream);
-    procedure BeginUpdate;
-    procedure EndUpdate;
-    procedure ClearLevel;
   published
-    property Info: TLevelInfo read fLevelInfo write SetLevelInfo;
-    property InteractiveObjects: TInteractiveObjects read fInteractiveObjects write SetInteractiveObjects;
-    property Terrains: TTerrains read fTerrains write SetTerrains;
-    property Steels: TSteels read fSteels write SetSteels;
-    property PreplacedLemmings: TPreplacedLemmingList read fPreplacedLemmings write fPreplacedLemmings;
+    property Info: TLevelInfo read fLevelInfo;
+    property InteractiveObjects: TInteractiveObjects read fInteractiveObjects;
+    property Terrains: TTerrains read fTerrains;
+    property Steels: TSteels read fSteels;
+    property PreplacedLemmings: TPreplacedLemmingList read fPreplacedLemmings;
   end;
 
   {
@@ -194,34 +136,6 @@ type
 implementation
 
 { TLevelInfo }
-
-procedure TLevelInfo.Assign(Source: TPersistent);
-// don't use this, it's very outdated
-var
-  L: TLevelInfo absolute Source;
-begin
-  if Source is TLevelInfo then
-  begin
-    ReleaseRate    := L.ReleaseRate;
-    LemmingsCount  := L.LemmingsCount;
-    RescueCount    := L.RescueCount;
-    TimeLimit      := L.TimeLimit;
-    ClimberCount   := L.ClimberCount;
-    FloaterCount   := L.FloaterCount;
-    BomberCount    := L.BomberCount;
-    BlockerCount   := L.BlockerCount;
-    BuilderCount   := L.BuilderCount;
-    BasherCount    := L.BasherCount;
-    MinerCount     := L.MinerCount;
-    DiggerCount    := L.DiggerCount;
-    GraphicSet     := L.GraphicSet;
-    GraphicSetEx   := L.GraphicSetEx;
-    SuperLemming   := L.SuperLemming;
-    ScreenPosition := L.ScreenPosition;
-    Title          := L.Title;
-  end
-  else inherited Assign(Source);
-end;
 
 procedure TLevelInfo.Clear;
 var
@@ -248,10 +162,7 @@ begin
   PlatformerCount := 0;
   StackerCount   := 0;
   ClonerCount := 0;
-  GraphicSet     := 0;
-  GraphicSetEx   := 0;
   LevelOptions   := 0;
-  SuperLemming   := 0;
   ScreenPosition := 0;
   ScreenYPosition := 0;
   Width := 1584;
@@ -270,105 +181,13 @@ end;
 
 { TLevel }
 
-procedure TLevel.AssignOddtable(Source: TLevel);
-var
-  i: Integer;
-  O, SO: TInteractiveObject;
-  T, ST: TTerrain;
-  S, SS: TSteel;
+constructor TLevel.Create;
 begin
-  with Info do
-  begin
-    LevelOptions := (LevelOptions and $71) or (Source.Info.LevelOptions and not $71);
-    Width := Source.Info.Width;
-    Height := Source.Info.Height;
-    GraphicSet := (GraphicSet and $FF00) or (Source.Info.GraphicSet and $FF);
-    GraphicSetEx := Source.Info.GraphicSetEx;
-    ScreenPosition := Source.Info.ScreenPosition;
-    ScreenYPosition := Source.Info.ScreenYPosition;
-    GraphicSetFile := Source.Info.GraphicSetFile;
-    GraphicMetaFile := Source.Info.GraphicMetaFile;
-    GraphicSetName := Source.Info.GraphicSetName;
-    VgaspecFile := Source.Info.VgaspecFile;
-    VgaspecX := Source.Info.VgaspecX;
-    VgaspecY := Source.Info.VgaspecY;
-    SetLength(Info.WindowOrder, Length(Source.Info.WindowOrder));
-    for i := 0 to Length(Source.Info.WindowOrder)-1 do
-      Info.WindowOrder[i] := Source.Info.WindowOrder[i];
-  end;
-
-  InteractiveObjects.Clear;
-  for i := 0 to Source.InteractiveObjects.Count-1 do
-  begin
-    O := InteractiveObjects.Add;
-    SO := Source.InteractiveObjects[i];
-    O.Assign(SO);
-  end;
-
-  Terrains.Clear;
-  for i := 0 to Source.Terrains.Count-1 do
-  begin
-    T := Terrains.Add;
-    ST := Source.Terrains[i];
-    T.Assign(ST);
-  end;
-
-  Steels.Clear;
-  for i := 0 to Source.Steels.Count-1 do
-  begin
-    S := Steels.Add;
-    SS := Source.Steels[i];
-    S.Assign(SS);
-  end;
-end;
-
-procedure TLevel.Assign(Source: TPersistent);
-var
-  L: TLevel absolute Source;
-begin
-  if Source is TLevel then
-  begin
-    BeginUpdate;
-    try
-      Info := L.Info;
-      InteractiveObjects := L.InteractiveObjects;
-      Terrains := L.Terrains;
-      Steels := L.Steels;
-    finally
-      EndUpdate;
-    end;
-  end
-  else inherited Assign(Source)
-end;
-
-procedure TLevel.BeginUpdate;
-begin
-{ TODO : a little more protection for beginupdate endupdate }
-  Inc(fUpdateCounter);
-  if fUpdateCounter = 1 then
-  begin
-    //fInteractiveObjects.BeginUpdate;
-    //fTerrains.BeginUpdate;
-    //fSteels.BeginUpdate;
-  end;
-end;
-
-procedure TLevel.ClearLevel;
-begin
-  //fLevelInfo.Clear;
-  fInteractiveObjects.Clear;
-  fTerrains.Clear;
-  fSteels.Clear;
-  fPreplacedLemmings.Clear;
-end;
-
-constructor TLevel.Create(aOwner: TComponent);
-begin
-  inherited Create(aOwner);
-  fLevelInfo := DoCreateLevelInfo;
-  fInteractiveObjects := DoCreateInteractiveObjects;
-  fTerrains := DoCreateTerrains;
-  fSteels := DoCreateSteels;
+  inherited;
+  fLevelInfo := TLevelInfo.Create;
+  fInteractiveObjects := TInteractiveObjects.Create;
+  fTerrains := TTerrains.Create;
+  fSteels := TSteels.Create;
   fPreplacedLemmings := TPreplacedLemmingList.Create;
 end;
 
@@ -378,79 +197,8 @@ begin
   fInteractiveObjects.Free;
   fTerrains.Free;
   fSteels.Free;
-  fPreplacedLemmings.Create;
+  fPreplacedLemmings.Free;
   inherited Destroy;
-end;
-
-function TLevel.DoCreateInteractiveObjects: TInteractiveObjects;
-begin
-  Result := TInteractiveObjects.Create;
-end;
-
-function TLevel.DoCreateLevelInfo: TLevelInfo;
-begin
-  Result := TLevelInfo.Create;
-end;
-
-function TLevel.DoCreateSteels: TSteels;
-begin
-  Result := TSteels.Create;
-end;
-
-function TLevel.DoCreateTerrains: TTerrains;
-begin
-  Result := TTerrains.Create;
-end;
-
-procedure TLevel.EndUpdate;
-begin
-  if fUpdateCounter > 0 then
-  begin
-    Dec(fUpdateCounter);
-    if fUpdateCounter = 0 then
-    begin
-      //fInteractiveObjects.EndUpdate;
-      //fTerrains.EndUpdate;
-      //fSteels.EndUpdate;
-    end;
-  end;
-end;
-
-procedure TLevel.SaveToFile(const aFileName: string);
-var
-  F: TFileStream;
-begin
-  F := TFileStream.Create(aFileName, fmCreate);
-  try
-     SaveToStream(F);
-  finally
-    F.Free;
-  end;
-end;
-
-procedure TLevel.SaveToStream(S: TStream);
-begin
-  S.WriteComponent(Self);
-end;
-
-procedure TLevel.SetLevelInfo(const Value: TLevelInfo);
-begin
-  fLevelInfo.Assign(Value);
-end;
-
-procedure TLevel.SetInteractiveObjects(Value: TInteractiveObjects);
-begin
-  fInteractiveObjects.Assign(Value);
-end;
-
-procedure TLevel.SetTerrains(Value: TTerrains);
-begin
-  fTerrains.Assign(Value);
-end;
-
-procedure TLevel.SetSteels(Value: TSteels);
-begin
-  fSteels.Assign(Value);
 end;
 
 end.
