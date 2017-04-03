39f33a707896de520e5eba46bd1b79185525e8a9
Namida <hayanninja@yandex.com>
2017-04-04 06:30:09 +1200

    Fixed bugs: Mac-style rotation is always treated as on in newer NXPs (due to reading a wrong setting that *will* always be on in newer NXPs); message box just saying "0" pops up on older NXPs when starting a level.

--- GameWindow.pas
+++ GameWindow.pas
@@ -329,12 +329,9 @@ begin
     else begin
       MusicIndex := GameParams.Info.dLevel;
 
-      if GameParams.SysDat.Options4 and 2 <> 0 then
-      begin
+      if GameParams.SysDat.Options4 and 4 <> 0 then
         for i := 0 to GameParams.Info.dSection-1 do
           MusicIndex := MusicIndex + TBaseDosLevelSystem(GameParams.Style.LevelSystem).GetLevelCount(i);
-      end else
-        ShowMessage(IntToStr(GameParams.SysDat.Options4));
     end;
 
   if SL.Count > 0 then
