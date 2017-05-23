522b95e96c93004174974a6d497c4a6fa83c2bc5
Namida <hayanninja@yandex.com>
2017-05-24 03:37:03 +1200

    Removed the no-longer-needed lemdata resource. This in turn makes LemResourceBuilder no longer needed, so removed it too.

--- LemRes.pas
+++ LemRes.pas
@@ -6,7 +6,7 @@ interface
 
   {$resource lemmixresources.res}             // lemmixresources contains the cursor bitmaps
 
-  {$resource 'data\lemdata.res'}        // music has been integrated into this to simplify code
+//  {$resource 'data\lemdata.res'}        // music has been integrated into this to simplify code
 //  {$resource 'sounds\lemsounds.res'}    // lemsounds contains the sounds (wav)
   {$resource 'bass.res'}
   {$resource 'explode.res'}
