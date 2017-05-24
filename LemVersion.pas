dd3d348b37275407c1725fd8aad90f750ce18ccd
Namida <hayanninja@yandex.com>
2017-05-25 02:32:23 +1200

    Updated commit ID, reconverted Lemmings Plus Omega both to test the conversion tool and because of the circuit / metal unlock button change.

--- LemVersion.pas
+++ LemVersion.pas
@@ -12,7 +12,7 @@ const
   CORE_VERSION = 13;
   FEATURES_VERSION = 17;
   HOTFIX_VERSION = 0;
-  COMMIT_ID = '439a7e7';  // empty string is handled, and is uppercased when needed so don't need to do manually anymore :D
+  COMMIT_ID = '89a1442';  // empty string is handled, and is uppercased when needed so don't need to do manually anymore :D
 
 function MakeVersionString(aFormat, aCore, aFeature, aHotfix: Integer): String;
 function MakeVersionID(aFormat, aCore, aFeature, aHotfix: Integer): Int64;
