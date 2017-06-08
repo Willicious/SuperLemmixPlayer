6c11d81fed67ab77b0bbdf312adbd1dc3a37e25e
Namida <hayanninja@yandex.com>
2017-06-09 00:36:08 +1200

    Updated commit ID. Also for some reason there's some changes to GSConvert's files.

--- LemVersion.pas
+++ LemVersion.pas
@@ -12,7 +12,7 @@ const
   CORE_VERSION = 14;
   FEATURES_VERSION = 19;
   HOTFIX_VERSION = 0;
-  COMMIT_ID = 'b1d4aa4';  // empty string is handled, and is uppercased when needed so don't need to do manually anymore :D
+  COMMIT_ID = 'c474af8';  // empty string is handled, and is uppercased when needed so don't need to do manually anymore :D
 
 function MakeVersionString(aFormat, aCore, aFeature, aHotfix: Integer): String;
 function MakeVersionID(aFormat, aCore, aFeature, aHotfix: Integer): Int64;
