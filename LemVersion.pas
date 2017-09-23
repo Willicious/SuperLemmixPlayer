819485d0f15f8c38291df8cb90afd104751a4e8e
Stephan <st.neupert@web.de>
2017-09-23 18:08:53 +0200

    Updated version number to 11.15.20 for release of the experimental version.

--- LemVersion.pas
+++ LemVersion.pas
@@ -9,8 +9,8 @@ uses
 
 const
   FORMAT_VERSION = 11;
-  CORE_VERSION = 14;
-  FEATURES_VERSION = 19;
+  CORE_VERSION = 15;
+  FEATURES_VERSION = 20;
   HOTFIX_VERSION = 0;
   //COMMIT_ID = 'c474af8';  // empty string is handled, and is uppercased when needed so don't need to do manually anymore :D
 
