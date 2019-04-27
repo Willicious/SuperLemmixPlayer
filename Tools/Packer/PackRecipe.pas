f245911c6dc9ff3d048f6fd4f29a21d78df0606d
Namida Verasche <namida.verasche@gmail.com>
2019-04-27 17:31:38 +1200

    Even more! :D

--- Tools/Packer/PackRecipe.pas
+++ Tools/Packer/PackRecipe.pas
@@ -5,26 +5,30 @@ interface
 uses
   Classes,
   SysUtils,
+  LemNeoLevelPack, LemLevel,
   Generics.Collections;
 
 type
   TStyleInclude = (siFull, siPartial, siNone);
 
-  TRecipePack = record
-    PackFolder: String;
-    NewStylesInclude: TStyleInclude;
-    NewMusicInclude: Boolean;
+  TRecipePack = class
+    public
+      PackFolder: String;
+      NewStylesInclude: TStyleInclude;
+      NewMusicInclude: Boolean;
   end;
 
-  TRecipeStyle = record
-    AutoAdded: Boolean;
-    StyleName: String;
-    Include: TStyleInclude;
+  TRecipeStyle = class
+    public
+      AutoAdded: Boolean;
+      StyleName: String;
+      Include: TStyleInclude;
   end;
 
-  TRecipeFile = record
-    AutoAdded: Boolean;
-    FilePath: String;
+  TRecipeFile = class
+    public
+      AutoAdded: Boolean;
+      FilePath: String;
   end;
 
   TPackageRecipe = class
@@ -36,6 +40,12 @@ type
       fPacks: TList<TRecipePack>;
       fStyles: TList<TRecipeStyle>;
       fFiles: TList<TRecipeFile>;
+
+      procedure ClearAutoAdds;
+      procedure BuildAutoAdds;
+
+      procedure BuildPackAutoAdds(aPack: TRecipePack);
+      procedure BuildPackAutoAddLists(aPack: TRecipePack; var aObjects, aTerrains, aBackgrounds, aThemes, aMusic: TStringList);
     public
       constructor Create;
       destructor Destroy; override;
@@ -47,18 +57,21 @@ type
       property PackageType: String read fPackageType write fPackageType;
       property PackageVersion: String read fPackageVersion write fPackageVersion;
       
-      property Packs: TList<TRecipePack> read fPacks;
-      property Styles: TList<TRecipeStyle> read fStyles;
-      property Files: TList<TRecipeFile> read fFiles;
+      property Packs: TObjectList<TRecipePack> read fPacks;
+      property Styles: TObjectList<TRecipeStyle> read fStyles;
+      property Files: TObjectList<TRecipeFile> read fFiles;
   end;
 
 implementation
 
+uses
+  GameControl;
+
 constructor TPackageRecipe.Create;
 begin
-  fPacks := TList<TRecipePack>.Create;
-  fStyles := TList<TRecipeStyle>.Create;
-  fFiles := TList<TRecipeFile>.Create;
+  fPacks := TObjectList<TRecipePack>.Create;
+  fStyles := TObjectList<TRecipeStyle>.Create;
+  fFiles := TObjectList<TRecipeFile>.Create;
 end;
 
 destructor TPackageRecipe.Destroy;
@@ -93,6 +106,8 @@ begin
 
         if UpperCase(SubSL[0]) = 'PACK' then
         begin
+          NewPack := TRecipePack.Create;
+        
           NewPack.PackFolder := SubSL.Values['FOLDER'];
 
           S := UpperCase(SubSL.Values['NEW_STYLES_INCLUDE']);
@@ -108,6 +123,8 @@ begin
           Packs.Add(NewPack);  
         end else if UpperCase(SubSL[0]) = 'STYLE' then
         begin
+          NewStyle := TRecipeStyle.Create;
+        
           NewStyle.AutoAdded := false;
           NewStyle.StyleName := SubSL.Values['NAME'];
 
@@ -121,8 +138,12 @@ begin
           Styles.Add(NewStyle);
         end else if UpperCase(SubSL[0]) = 'FILE' then
         begin
+          NewFile := TRecipeFile.Create;
+          
           NewFile.AutoAdded := false;
           NewFile.FilePath := SubSL.Values['PATH'];
+
+          Files.Add(NewFile);
         end else if UpperCase(SubSL[0]) = 'META' then
         begin
           fPackageName := SubSL.Values['NAME'];
@@ -216,6 +237,118 @@ begin
   finally
     SL.Free;
     SubSL.Free;
+  end;
+end;
+
+procedure TPackageRecipe.ClearAutoAdds;
+var
+  i: Integer;
+begin
+  for i := fStyles.Count-1 downto 0 do
+    if fStyles[i].AutoAdded then
+      fStyles.Delete(i);
+
+  for i := fFiles.Count-1 downto 0 do
+    if fFiles[i].AutoAdded then
+      fFiles.Delete(i);
+end;
+
+procedure TPackageRecipe.BuildAutoAdds;
+var
+  i: Integer;
+begin
+  for i := 0 to fPacks.Count-1 do
+    BuildPackAutoAdds(fPacks[i]);
+end;
+
+procedure TPackageRecipe.BuildPackAutoAdds(aPack: TRecipePack);
+begin
+
+end;
+
+procedure TPackageRecipe.BuildPackAutoAddLists(aPack: TRecipePack; var aObjects, aTerrains, aBackgrounds, aThemes, aMusic: TStringList);
+var
+  UsedObjects: TStringList;
+  UsedTerrain: TStringList;
+  UsedBackgrounds: TStringList;
+  UsedThemes: TStringList;
+  UsedMusic: TStringList;
+
+  i: Integer;
+  FoundPack: Boolean;
+
+  procedure RecursiveIdentify(aPack: TNeoLevelGroup);
+  var
+    i, n: Integer;
+    S: String;
+
+    Level: TLevel;
+  begin
+    for i := 0 to aPack.Children.Count-1 do
+      RecursiveIdentify(aPack.Children[i]);
+
+    for i := 0 to aPack.Levels.Count-1 do
+    begin
+      GameParams.SetLevel(aPack.Levels[i]);
+      GameParams.LoadCurrentLevel(true);
+      Level := GameParams.Level;
+      
+      S := Trim(Level.Info.MusicFile);
+      if (S <> '') and (S[1] <> '?') then
+        UsedMusic.Add(S);
+
+      UsedThemes.Add(Level.Info.GraphicSetName);
+
+      if (Level.Info.Background <> '') then
+        UsedBackgrounds.Add(Level.Info.Background);
+
+      for n := 0 to Level.InteractiveObjects.Count-1 do
+        UsedObjects.Add(Level.InteractiveObjects[n].Identifier);
+
+      for n := 0 to Level.Terrains.Count-1 do
+        UsedTerrain.Add(Level.Terrains[n].Identifier);
+    end;
+  end;
+begin
+  if (aPack.NewStylesInclude = siNone) and not aPack.NewMusicInclude then
+    Exit;
+
+  FoundPack := false;
+  for i := 0 to GameParams.BaseLevelPack.Children.Count-1 do
+    if GameParams.BaseLevelPack.Children[i].Path = aPack.PackFolder then
+    begin
+      GameParams.SetGroup(GameParams.BaseLevelPack.Children[i]);
+      FoundPack := true;
+    end;
+
+  if not FoundPack then
+    Exit;
+
+  UsedObjects := TStringList.Create;
+  UsedTerrain := TStringList.Create;
+  UsedBackgrounds := TStringList.Create;
+  UsedThemes := TStringList.Create;
+  UsedMusic := TStringList.Create;
+  try
+    UsedObjects.Duplicates := dupIgnore;
+    UsedTerrain.Duplicates := dupIgnore;
+    UsedBackgrounds.Duplicates := dupIgnore;
+    UsedThemes.Duplicates := dupIgnore;
+    UsedMusic.Duplicates := dupIgnore;
+  
+    RecursiveIdentify(GameParams.CurrentLevel.Group.ParentBasePack);
+
+    aObjects.Assign(UsedObjects);
+    aTerrains.Assign(UsedTerrain);
+    aBackgrounds.Assign(UsedBackgrounds);
+    aThemes.Assign(UsedThemes);
+    aMusic.Assign(UsedMusic);
+  finally
+    UsedObjects.Free;
+    UsedTerrain.Free;
+    UsedBackgrounds.Free;
+    UsedThemes.Free;
+    UsedMusic.Free;
   end;
 end;
 
