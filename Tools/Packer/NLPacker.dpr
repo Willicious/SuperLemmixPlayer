e64ea479b9ba2aec5e2f4000e4700e7b60d0a8b0
Namida Verasche <namida.verasche@gmail.com>
2019-04-28 10:56:27 +1200

    Using the ".nx.zip" extension to uniquely identify NL zip files.

--- Tools/Packer/NLPacker.dpr
+++ Tools/Packer/NLPacker.dpr
@@ -34,7 +34,7 @@ begin
 
       OutputFile := ParamStr(2);
       if OutputFile = '' then
-        OutputFile := ChangeFileExt(ParamStr(1), '.zip')
+        OutputFile := ChangeFileExt(ParamStr(1), '.nx.zip')
       else if not TPath.IsPathRooted(OutputFile) then
         OutputFile := BasePath + OutputFile;
 
