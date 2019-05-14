969b64238996daecdb60e31d1001db53bfc9a735
Namida Verasche <namida.verasche@gmail.com>
2019-05-15 10:57:42 +1200

    DOM_xxx consts moved to a separate file. This allows LemGadgetsMeta to make use of these values.

--- LemGadgets.pas
+++ LemGadgets.pas
@@ -6,7 +6,7 @@ interface
 uses
   Math, Classes, GR32,
   Windows, Contnrs, SysUtils, LemTypes, LemCore,
-  LemGadgetsMeta, LemGadgetsModel,
+  LemGadgetsMeta, LemGadgetsModel, LemGadgetsConstants,
   LemGadgetAnimation,
   Generics.Collections;
 
@@ -171,43 +171,6 @@ type
     property Items[Index: Integer]: TGadget read GetItem; default;
   published
   end;
-
-const
-  DOM_NOOBJECT         = 65535;
-  DOM_NONE             = 0;
-  DOM_EXIT             = 1;
-  DOM_FORCELEFT        = 2; // left arm of blocker
-  DOM_FORCERIGHT       = 3; // right arm of blocker
-  DOM_TRAP             = 4; // triggered trap
-  DOM_WATER            = 5; // causes drowning
-  DOM_FIRE             = 6; // causes vaporizing
-  DOM_ONEWAYLEFT       = 7;
-  DOM_ONEWAYRIGHT      = 8;
-  DOM_STEEL            = 9;
-  DOM_BLOCKER          = 10; // the middle part of blocker
-  DOM_TELEPORT         = 11;
-  DOM_RECEIVER         = 12;
-  DOM_LEMMING          = 13;
-  DOM_PICKUP           = 14;
-  DOM_LOCKEXIT         = 15;
-  DOM_SKETCH           = 16; // replaces DOM_SECRET, shouldn't be in LVL files, and gets hidden if it is
-  DOM_BUTTON           = 17;
-  //DOM_RADIATION        = 18;
-  DOM_ONEWAYDOWN       = 19;
-  DOM_UPDRAFT          = 20;
-  DOM_FLIPPER          = 21;
-  //DOM_SLOWFREEZE       = 22;
-  DOM_WINDOW           = 23;
-  //DOM_ANIMATION        = 24;
-  DOM_HINT             = 25;
-  //DOM_NOSPLAT          = 26;
-  DOM_SPLAT            = 27;
-  //DOM_TWOWAYTELE       = 28;
-  //DOM_SINGLETELE       = 29;
-  DOM_BACKGROUND       = 30;
-  DOM_TRAPONCE         = 31;
-  //DOM_BGIMAGE          = 32;
-  DOM_ONEWAYUP         = 33;
 
 implementation
 
