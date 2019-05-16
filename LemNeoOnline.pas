226b4d5f6458115a9343040d28a35c3fb31d1a1f
Namida Verasche <namida.verasche@gmail.com>
2019-05-16 22:51:22 +1200

    Bones of a TThread descendant for the download.

--- LemNeoOnline.pas
+++ LemNeoOnline.pas
@@ -2,6 +2,53 @@ unit LemNeoOnline;
 
 interface
 
+uses
+  Classes, SysUtils;
+
+type
+  TDownloadThread = class(TThread)
+    private
+      fSourceURL: String;
+      fStream: TStream;
+
+      fCancel: Boolean;
+      fComplete: Boolean;
+      fSuccess: Boolean;
+    protected
+      procedure Execute; override;
+    public
+      constructor Create(aSourceURL: String; aTargetStream: TStream);
+      destructor Destroy; override;
+
+      procedure Cancel;
+  end;
+
 implementation
+
+{ TDownloadThread }
+
+procedure TDownloadThread.Cancel;
+begin
+  fCancel := true;
+end;
+
+constructor TDownloadThread.Create(aSourceURL: String; aTargetStream: TStream);
+begin
+  fCancel := false;
+  fSourceURL := aSourceURL;
+  fStream := aTargetStream;
+end;
+
+destructor TDownloadThread.Destroy;
+begin
+
+  inherited;
+end;
+
+procedure TDownloadThread.Execute;
+begin
+  inherited;
+
+end;
 
 end.
