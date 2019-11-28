61fe1e1e206071def5064d5dac9e0e8b770000de
Namida Verasche <namida.verasche@gmail.com>
2019-11-28 19:57:42 +1300

    Version number.

--- LemVersion.pas
+++ LemVersion.pas
@@ -9,8 +9,8 @@ uses
 
 const
   FORMAT_VERSION = 12;
-  CORE_VERSION = 7;
-  FEATURES_VERSION = 1;
+  CORE_VERSION = 8;
+  FEATURES_VERSION = 0;
   HOTFIX_VERSION = 0; // or RC version
 
   function COMMIT_ID: String;
