3183d117c0eb8701829d24501c20ea118b58bdd6
Namida Verasche <namida.verasche@gmail.com>
2019-12-15 14:05:32 +1300

    Style zipper has better sound file handling.

--- Tools/StyleZipper/StyleZipper.dpr
+++ Tools/StyleZipper/StyleZipper.dpr
@@ -7,21 +7,71 @@ program StyleZipper;
 uses
   Zip,
   Classes,
-  System.SysUtils;
+  StrUtils,
+  SysUtils;
+
+const
+  DEFAULT_SOUNDS: array[0..27] of string =
+   ('chain',
+    'changeop',
+    'chink',
+    'die',
+    'door',
+    'electric',
+    'explode',
+    'failure',
+    'fire',
+    'glug',
+    'letsgo',
+    'mousepre',
+    'ohno',
+    'oing2',
+    'slurp',
+    'splash',
+    'splat',
+    'success',
+    'tenton',
+    'thud',
+    'thunk',
+    'timeup',
+    'ting',
+    'vacuusux',
+    'weedgulp',
+    'wrench',
+    'yippee',
+    'zombie');
+
+  SOUND_EXTS: array[0..4] of string = ('.ogg', '.wav', '.aiff', '.aif', '.mp3');
 
   function AppPath: String;
   begin
     Result := IncludeTrailingPathDelimiter(ExtractFilePath(ParamStr(0)));
   end;
 
+  function IsDefaultSound(aName: String): Boolean;
+  var
+    i: Integer;
+  begin
+    Result := true;
+    for i := 0 to Length(DEFAULT_SOUNDS)-1 do
+      if DEFAULT_SOUNDS[i] = aName then Exit;
+
+    Result := false;
+  end;
+
   procedure ZipStyle(aName: String);
   var
     Zip: TZipFile;
     Base: String;
+    Sounds: TStringList;
+    ObjSL: TStringList;
+    i: Integer;
 
     procedure AddRecursive(aRelPath: String);
     var
       SearchRec: TSearchRec;
+      i: Integer;
+      Line: String;
     begin
       if FindFirst(Base + aRelPath + '*', faDirectory, SearchRec) = 0 then
       begin
@@ -29,24 +79,60 @@ uses
           if (SearchRec.Name = '..') or (SearchRec.Name = '.') then Continue;
 
           if (SearchRec.Attr and faDirectory) = 0 then
-            Zip.Add(Base + aRelPath + SearchRec.Name, 'styles/' + aName + '/' + aRelPath + SearchRec.Name)
-          else
+          begin
+            Zip.Add(Base + aRelPath + SearchRec.Name, 'styles/' + aName + '/' + aRelPath + SearchRec.Name);
+            if CompareText(ExtractFileExt(SearchRec.Name), '.nxmo') = 0 then
+            begin
+              ObjSL.LoadFromFile(Base + aRelPath + SearchRec.Name);
+              for i := 0 to ObjSL.Count-1 do
+              begin
+                Line := TrimLeft(ObjSL[i]);
+                if CompareText(LeftStr(Line, 5), 'SOUND') = 0 then
+                  Sounds.Add(RightStr(Line, Length(Line) - 6));
+              end;
+            end;
+          end else
             AddRecursive(aRelPath + SearchRec.Name + '\');
         until FindNext(SearchRec) <> 0;
         FindClose(SearchRec);
       end;
     end;
+
+    procedure AddSound(aName: String);
+    var
+      i: Integer;
+    begin
+      for i := 0 to Length(SOUND_EXTS) - 1 do
+        if FileExists(AppPath + 'sound\' + aName + SOUND_EXTS[i]) then
+        begin
+          Zip.Add(AppPath + 'sound\' + aName + SOUND_EXTS[i], 'sound\' + aName + SOUND_EXTS[i]);
+          Break;
+        end;
+    end;
   begin
     ForceDirectories(AppPath + 'style_zips\');
 
     Zip := TZipFile.Create;
+    Sounds := TStringList.Create;
+    ObjSL := TStringList.Create;
     try
       Zip.Open(AppPath + 'style_zips\' + aName + '.zip', zmWrite);
       Base := AppPath + 'styles\' + aName + '\';
       AddRecursive('');
+
+      for i := 0 to Sounds.Count-1 do
+        if not IsDefaultSound(Sounds[i]) then
+          AddSound(Sounds[i]);
+
+      if aName = 'default' then
+        for i := 0 to Length(DEFAULT_SOUNDS)-1 do
+          AddSound(DEFAULT_SOUNDS[i]);
+
       Zip.Close;
     finally
       Zip.Free;
+      Sounds.Free;
+      ObjSL.Free;
     end;
   end;
 
@@ -118,8 +204,7 @@ uses
 begin
   try
     ZipStyles;
-    MakeDirectoryZip('styles|sound', 'nx_all_styles.zip');
-    MakeDirectoryZip('sound', 'nx_sounds.zip');
+    MakeDirectoryZip('styles|sound', '_all_styles.zip');
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
