cf39f146fbd60dfeaab69f6640b5c62fe3c6ac2a
Namida Verasche <namida.verasche@gmail.com>
2020-06-05 12:42:39 +1200

    Sketch graphics.

--- MakeSymLinks.bat
+++ MakeSymLinks.bat
@@ -2,6 +2,7 @@ REM Symlinks can safely be deleted if desired, and will not be created if the fo
 mklink /J bin\data data\external\data
 mklink /J bin\gfx data\external\gfx
 mklink /J bin\music data\external\music
+mklink /J bin\sketches data\external\sketches
 mklink /J bin\sound data\external\sound
 mklink /J bin\styles data\external\styles
 mklink /H bin\bass.dll data\external\bass.dll
