b9431e4009a90faeccb76243caa17da07087e8f6
Namida Verasche <namida.verasche@gmail.com>
2020-10-28 18:48:07 +1300

    Needs testing of course, but that *should* be the actual projectile arc working.

--- LemProjectile.pas
+++ LemProjectile.pas
@@ -9,11 +9,42 @@ uses
   Classes, SysUtils, Math, Types;
 
 type
+  TProjectileGraphic =
+   (
+     pgSpearFlat,
+     pgSpearSlightTLBR, pgSpearSlightBLTR,
+     pgSpear45TLBR, pgSpear45BLTR,
+     pgSpearSteepTLBR, pgSpearSteepBLTR,
+     pgGrenade
+   );
+
+const
+  PROJECTILE_FLIP: array[TProjectileGraphic] of TProjectileGraphic =
+  (
+    pgSpearFlat,
+    pgSpearSlightBLTR, pgSpearSlightTLBR,
+    pgSpear45BLTR, pgSpear45TLBR,
+    pgSpearSteepBLTR, pgSpearSteepTLBR,
+    pgGrenade
+  );
+
+  PROJECTILE_GRAPHIC_RECTS: array[TProjectileGraphic] of TRect =
+  (
+    (Left: 0; Top: 0; Right: 14; Bottom: 2),
+    (Left: 0; Top: 3; Right: 12; Bottom: 9),
+    (Left: 0; Top: 45; Right: 12; Bottom: 51),
+    (Left: 0; Top: 10; Right: 10; Bottom: 20),
+    (Left: 0; Top: 34; Right: 10; Bottom: 44),
+    (Left: 0; Top: 21; Right: 6; Bottom: 33),
+    (Left: 7; Top: 21; Right: 13; Bottom: 33),
+    (Left: 0; Top: 52; Right: 4; Bottom: 57)
+  );
+
+type
   TProjectilePointArray = array of TPoint;
 
   TProjectile = class
     private
-
       fX: Integer;
       fY: Integer;
 
@@ -26,22 +57,33 @@ type
 
       fPhysicsMap: TBitmap32;
       fLemming: TLemming;
+      fLemmingIndex: Integer;
 
       fIsSpear: Boolean;
       fIsGrenade: Boolean;
 
       constructor Create(aPhysicsMap: TBitmap32; aLemming: TLemming);
+
+      procedure Fire;
+
+      function GetGraphic: TProjectileGraphic;
+      function GetGraphicHotspot: TPoint;
     public
       constructor CreateSpear(aPhysicsMap: TBitmap32; aLemming: TLemming);
       constructor CreateGrenade(aPhysicsMap: TBitmap32; aLemming: TLemming);
 
-      procedure Fire;
       function Update: TProjectilePointArray;
+      procedure Relink(aPhysicsMap: TBitmap32; aList: TLemmingList);
+
+      procedure Assign(aSrc: TProjectile);
 
       property X: Integer read fX;
       property Y: Integer read fY;
 
       property Hit: Boolean read fHit;
+
+      property Hotspot: TPoint read GetGraphicHotspot;
+      property Graphic: TProjectileGraphic read GetGraphic;
   end;
 
 implementation
@@ -49,64 +91,116 @@ implementation
 const
   PROJECTILE_HORIZONTAL_MOMENTUM = 9;
 
-  Y_CHANGE_TO_NEXT_X: array[0..332] of Integer =
+  SPEAR_OFFSETS: array[0..2] of TPoint =
   (
-     -1,  0, -1, -1,  0, -1, -1, // initial - slight up angle
-      0, -1,  0, -1, -1,  0, -1,
-      0, -1,  0, -1,  0, -1,  0,
-     -1,  0, -1,  0, -1,  0,  0,
-     -1,  0, -1,  0,  0, -1,  0,
-      0, -1,  0,  0, -1,  0,  0, // 35 - set to horizontal graphic
-      0, -1,  0,  0,  0, -1,  0,
-      0,  0,  0, -1,  0,  0,  0,
-      0,  0,  0, -1,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  1,  0,  0,  0,
-      0,  0,  0,  1,  0,  0,  0,
-      0,  1,  0,  0,  0,  1,  0,
-      0,  0,  1,  0,  0,  1,  0,
-      0,  1,  0,  0,  1,  0,  1, // 105 - set to slight down angle
-      0,  0,  1,  0,  1,  0,  1,
-      0,  1,  0,  1,  0,  1,  0,
-      1,  0,  1,  1,  0,  1,  0,
-      1,  1,  0,  1,  1,  0,  1,
-      1,  0,  1,  1,  1,  0,  1,
-      1,  1,  0,  1,  1,  1,  1, // 147 - set to 45 angle
-      0,  1,  1,  1,  1,  1,  1,
-      0,  1,  1,  1,  1,  1,  1,
-      1,  1,  1,  1,  1,  1,  1,
-      1,  1,  1,  1,  1,  1,  1,
-      2,  1,  1,  1,  1,  1,  1,
-      2,  1,  1,  1,  1,  2,  1,
-      1,  1,  2,  1,  1,  1,  2,
-      1,  1,  2,  1,  1,  2,  1,
-      1,  2,  1,  2,  1,  1,  2,
-      1,  2,  1,  2,  1,  2,  1,
-      2,  1,  2,  1,  2,  1,  2,
-      2,  1,  2,  1,  2,  2,  1,
-      2,  2,  1,  2,  2,  1,  2, // 231 - set to strong down angle
-      2,  2,  1,  2,  2,  2,  1,
-      2,  2,  2,  2,  1,  2,  2,
-      2,  2,  2,  2,  1,  2,  2,
-      2,  2,  2,  2,  2,  2,  2,
-      2,  2,  2,  2,  2,  2,  2,
-      2,  2,  2,  2,  3,  2,  2,
-      2,  2,  2,  2,  2,  3,  2,
-      2,  2,  2,  3,  2,  2,  2,
-      3,  2,  2,  3,  2,  2,  3,
-      2,  2,  3,  2,  2,  3,  2,
-      3,  2,  2,  3,  2,  3,  2,
-      3,  2,  3,  2,  3,  2,  3,
-      2,  3,  2,  3
+    (X: 2; Y: -3),
+    (X: 3; Y: -3),
+    (X: 4; Y: -1)
   );
 
+{$REGION '  Y offset array'}
+    Y_CHANGE_TO_NEXT_X: array[0..332] of Integer =
+    (
+       -1,  0, -1, -1,  0, -1, -1, // initial - slight up angle
+        0, -1,  0, -1, -1,  0, -1,
+        0, -1,  0, -1,  0, -1,  0,
+       -1,  0, -1,  0, -1,  0,  0,
+       -1,  0, -1,  0,  0, -1,  0,
+        0, -1,  0,  0, -1,  0,  0, // 35 - set to horizontal graphic
+        0, -1,  0,  0,  0, -1,  0,
+        0,  0,  0, -1,  0,  0,  0,
+        0,  0,  0, -1,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  1,  0,  0,  0,
+        0,  0,  0,  1,  0,  0,  0,
+        0,  1,  0,  0,  0,  1,  0,
+        0,  0,  1,  0,  0,  1,  0,
+        0,  1,  0,  0,  1,  0,  1, // 105 - set to slight down angle
+        0,  0,  1,  0,  1,  0,  1,
+        0,  1,  0,  1,  0,  1,  0,
+        1,  0,  1,  1,  0,  1,  0,
+        1,  1,  0,  1,  1,  0,  1,
+        1,  0,  1,  1,  1,  0,  1,
+        1,  1,  0,  1,  1,  1,  1, // 147 - set to 45 angle
+        0,  1,  1,  1,  1,  1,  1,
+        0,  1,  1,  1,  1,  1,  1,
+        1,  1,  1,  1,  1,  1,  1,
+        1,  1,  1,  1,  1,  1,  1,
+        2,  1,  1,  1,  1,  1,  1,
+        2,  1,  1,  1,  1,  2,  1,
+        1,  1,  2,  1,  1,  1,  2,
+        1,  1,  2,  1,  1,  2,  1,
+        1,  2,  1,  2,  1,  1,  2,
+        1,  2,  1,  2,  1,  2,  1,
+        2,  1,  2,  1,  2,  1,  2,
+        2,  1,  2,  1,  2,  2,  1,
+        2,  2,  1,  2,  2,  1,  2, // 231 - set to strong down angle
+        2,  2,  1,  2,  2,  2,  1,
+        2,  2,  2,  2,  1,  2,  2,
+        2,  2,  2,  2,  1,  2,  2,
+        2,  2,  2,  2,  2,  2,  2,
+        2,  2,  2,  2,  2,  2,  2,
+        2,  2,  2,  2,  3,  2,  2,
+        2,  2,  2,  2,  2,  3,  2,
+        2,  2,  2,  3,  2,  2,  2,
+        3,  2,  2,  3,  2,  2,  3,
+        2,  2,  3,  2,  2,  3,  2,
+        3,  2,  2,  3,  2,  3,  2,
+        3,  2,  3,  2,  3,  2,  3,
+        2,  3,  2,  3
+    );
+{$ENDREGION}
+
 { TProjectile }
+
+procedure TProjectile.Assign(aSrc: TProjectile);
+begin
+  {fX: Integer;
+      fY: Integer;
+
+      fOffsetX: Integer;
+
+      fDX: Integer;
+
+      fFired: Boolean;
+      fHit: Boolean;
+
+      fPhysicsMap: TBitmap32;
+      fLemming: TLemming;
+      fLemmingIndex: Integer;
+
+      fIsSpear: Boolean;
+      fIsGrenade: Boolean;}
+
+  fX := aSrc.fX;
+  fY := aSrc.fY;
+
+  fOffsetX := aSrc.fOffsetX;
+
+  fDX := aSrc.fDX;
+
+  fFired := aSrc.fFired;
+  fHit := aSrc.fHit;
+
+  // don't assign physicsmap or lemming
+  fLemmingIndex := aSrc.fLemmingIndex;
+
+  fIsSpear := aSrc.fIsSpear;
+  fIsGrenade := aSrc.fIsGrenade;
+end;
 
 constructor TProjectile.Create(aPhysicsMap: TBitmap32; aLemming: TLemming);
 begin
   fPhysicsMap := aPhysicsMap;
   fLemming := aLemming;
+
+  if fLemming <> nil then
+  begin
+    fLemmingIndex := fLemming.LemIndex;
+    fDX := fLemming.LemDX;
+  end else
+    fLemmingIndex := -1;
 end;
 
 constructor TProjectile.CreateGrenade(aPhysicsMap: TBitmap32;
@@ -129,10 +223,83 @@ begin
   fFired := true;
 end;
 
+function TProjectile.GetGraphic: TProjectileGraphic;
+begin
+  if fIsGrenade then
+    Result := pgGrenade
+  else begin
+    if not fFired then
+      case fLemming.LemPhysicsFrame of
+        0..3: Result := pgSpearSteepBLTR;
+        4: Result := pgSpear45BLTR;
+        5: Result := pgSpearSlightBLTR;
+
+        else Result := pgSpearFlat; // shouldn't happen
+      end
+    else
+      case fOffsetX of
+        35..104:  Result := pgSpearFlat;
+        105..147: Result := pgSpearSlightTLBR;
+        148..230: Result := pgSpear45TLBR;
+        else if fOffsetX < 35 then Result := pgSpearSlightBLTR
+        else Result := pgSpearSteepTLBR;
+      end;
+
+    if fDX < 0 then
+      Result := PROJECTILE_FLIP[Result];
+  end;
+end;
+
+function TProjectile.GetGraphicHotspot: TPoint;
+var
+  CurGraphic: TProjectileGraphic;
+  ImgRect: TRect;
+  AtTop: Boolean;
+begin
+  CurGraphic := Graphic;
+  ImgRect := PROJECTILE_GRAPHIC_RECTS[CurGraphic];
+
+  if CurGraphic = pgGrenade then
+  begin
+    Result := Point(ImgRect.Width div 2, ImgRect.Height div 2);
+    Exit;
+  end;
+
+  case CurGraphic of
+    pgSpearFlat: AtTop := true;
+
+    pgSpearSlightTLBR,
+    pgSpear45TLBR,
+    pgSpearSteepTLBR:
+      AtTop := fDX < 0;
+
+    pgSpearSlightBLTR,
+    pgSpear45BLTR,
+    pgSpearSteepBLTR:
+      AtTop := fDX > 0;
+
+    else AtTop := false; // should never happen
+  end;
+
+  if fDX < 0 then
+    Result.X := 1
+  else
+    Result.X := ImgRect.Width - 2;
+
+  if AtTop then
+    Result.Y := 1
+  else
+    Result.Y := ImgRect.Height - 2;
+end;
+
+procedure TProjectile.Relink(aPhysicsMap: TBitmap32; aList: TLemmingList);
+begin
+  fLemming := aList[fLemmingIndex];
+  fPhysicsMap := aPhysicsMap;
+end;
+
 function TProjectile.Update: TProjectilePointArray;
 var
-  i: Integer;
-
   PosCount: Integer;
 
   procedure AddPos(dX, dY: Integer);
@@ -151,6 +318,9 @@ var
         fHit := true;
     end;
   end;
+
+var
+  i: Integer;
 begin
   if fHit then
     raise Exception.Create('TProjectile.Update called for a projectile after it collides with terrain');
@@ -210,6 +380,42 @@ begin
   end else begin
     // Move with lemming's hand
     SetLength(Result, 0);
+    fDX := fLemming.LemDX;
+
+    case fLemming.LemPhysicsFrame of
+      0..3: begin
+              fX := fLemming.LemX - (4 * fLemming.LemDX);
+              fY := fLemming.LemY - 4;
+
+              if fIsSpear then
+              begin
+                fX := fX + SPEAR_OFFSETS[0].X;
+                fY := fY + SPEAR_OFFSETS[0].Y;
+              end;
+            end;
+      4: begin
+           fX := fLemming.LemX - (3 * fLemming.LemDX);
+           fY := fLemming.LemY - 7;
+
+           if fIsSpear then
+           begin
+             fX := fX + SPEAR_OFFSETS[1].X;
+             fY := fY + SPEAR_OFFSETS[1].Y;
+           end;
+         end;
+      5: begin
+           fX := fLemming.LemX + (1 * fLemming.LemDX);
+           fY := fLemming.LemY - 9;
+
+           if fIsSpear then
+           begin
+             fX := fX + SPEAR_OFFSETS[2].X;
+             fY := fY + SPEAR_OFFSETS[2].Y;
+           end;
+
+           Fire;
+         end;
+    end;
   end;
 end;
 
