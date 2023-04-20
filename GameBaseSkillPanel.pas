Willicious/SuperLemmixPlayer

commit 500e6efe8cbffebb2d888505675bd0da3692846f

Fixes bug which redraws selected graphic over pause button after backwards skips

GameBaseSkillPanel.pas
@@ -1098,10 +1098,9 @@ begin
   end;
   if fButtonRects[aButton].Left <= 0 then Exit;
 
+  RemoveHighlight(aButton);
   if Highlight then
     DrawHighlight(aButton)
-  else
-    RemoveHighlight(aButton);
 end;
 
 procedure TBaseSkillPanel.DrawHighlight(aButton: TSkillPanelButton);

commit f3c21c7031951751c51ffbdc20eda27231b6b9aa

Fixed bug which caused the skill panel button sound to not cue (itself caused by previous bugfix in 500e6efe8)

GameBaseSkillPanel.pas
@@ -1090,6 +1090,8 @@ begin
   if aButton = spbNone then Exit;
   if (aButton <= LAST_SKILL_BUTTON) then
   begin
+    if (fLastHighlitSkill <> spbNone) and (fLastHighlitSkill <> fHighlitSkill) then
+    SoundManager.PlaySound(SFX_SKILLBUTTON);
     if (fHighlitSkill = aButton) and Highlight then Exit;
     if (fHighlitSkill = spbNone) and not Highlight then Exit;
   end;
@@ -1097,7 +1099,7 @@ begin
 
   RemoveHighlight(aButton);
   if Highlight then
-  DrawHighlight(aButton);
+    DrawHighlight(aButton);
 end;
 
 procedure TBaseSkillPanel.DrawHighlight(aButton: TSkillPanelButton);
@@ -1108,8 +1110,6 @@ begin
   begin
     BorderRect := fButtonRects[aButton];
     fHighlitSkill := aButton;
-    if (fLastHighlitSkill <> spbNone) and (fLastHighlitSkill <> fHighlitSkill) then
-    SoundManager.PlaySound(SFX_SKILLBUTTON);
   end else
   BorderRect := fButtonRects[aButton];
 