c5186f460543e50767d6642c23edbbe9870c4c4c
Willicious <will3music@gmail.com>
2023-05-29 20:50:54 +0100

    Skill panel numbers made shorter so that skill icons have more button real estate; this is a trial, we may want to make them bigger again

--- GameBaseSkillPanel.pas
+++ GameBaseSkillPanel.pas
@@ -690,25 +690,25 @@ begin
     //////////////////////////////////////////////////////////
 
     // Walker, Jumper, Shimmier, Slider, Climber, - all simple
-    DrawAnimationFrame(fSkillIcons[spbWalker], WALKING, 1, 6, 21);
-    DrawAnimationFrame(fSkillIcons[spbJumper], JUMPING, 0, 6, 20);
-    DrawAnimationFrame(fSkillIcons[spbShimmier], SHIMMYING, 1, 7, 20);
+    DrawAnimationFrame(fSkillIcons[spbWalker], WALKING, 1, 6, 20);
+    DrawAnimationFrame(fSkillIcons[spbJumper], JUMPING, 0, 6, 19);
+    DrawAnimationFrame(fSkillIcons[spbShimmier], SHIMMYING, 1, 7, 19);
     DrawAnimationFrame(fSkillIcons[spbSlider], SLIDING_RTL, 0, 5, 21);
-    DrawAnimationFrame(fSkillIcons[spbClimber], CLIMBING, 3, 10, 22);
+    DrawAnimationFrame(fSkillIcons[spbClimber], CLIMBING, 3, 10, 20);
 
     // Swimmer - we need to draw the background water
-    DrawAnimationFrame(fSkillIcons[spbSwimmer], SWIMMING, 2, 8, 19);
+    DrawAnimationFrame(fSkillIcons[spbSwimmer], SWIMMING, 2, 8, 18);
     Outline(fSkillIcons[spbSwimmer]);
     TempBmp.Assign(fSkillIcons[spbSwimmer]);
     fSkillIcons[spbSwimmer].Clear(0);
-    fSkillIcons[spbSwimmer].FillRect(0, 17 * ResMod, 15 * ResMod, 23 * ResMod, $FF000000);
-    fSkillIcons[spbSwimmer].FillRect(0, 18 * ResMod, 15 * ResMod, 23 * ResMod, $FF0000FF);
+    fSkillIcons[spbSwimmer].FillRect(0, 17 * ResMod, 15 * ResMod, 22 * ResMod, $FF000000);
+    fSkillIcons[spbSwimmer].FillRect(0, 18 * ResMod, 15 * ResMod, 22 * ResMod, $FF0000FF);
     TempBmp.DrawTo(fSkillIcons[spbSwimmer]);
 
     // Floater, Glider, Disarmer - all simple
-    DrawAnimationFrame(fSkillIcons[spbFloater], UMBRELLA, 4, 7, 26);
-    DrawAnimationFrame(fSkillIcons[spbGlider], GLIDING, 4, 7, 26);
-    DrawAnimationFrame(fSkillIcons[spbDisarmer], FIXING, 6, 4, 21);
+    DrawAnimationFrame(fSkillIcons[spbFloater], UMBRELLA, 4, 7, 23);
+    DrawAnimationFrame(fSkillIcons[spbGlider], GLIDING, 4, 7, 25);
+    DrawAnimationFrame(fSkillIcons[spbDisarmer], FIXING, 6, 4, 20);
 
     //Timebomber has its own graphic
     if GameParams.HighResolution then
@@ -716,42 +716,41 @@ begin
     else
       TPngInterface.LoadPngFile(AppPath + SFGraphicsPanel + 'icon_timebomber.png', fIconBMP);
 
-      fIconBmp.DrawTo(fSkillIcons[spbTimebomber], -1 * ResMod, 8 * ResMod);
+    fIconBmp.DrawTo(fSkillIcons[spbTimebomber], -1 * ResMod, 7 * ResMod);
 
     // Bomber is drawn resized
-    DrawAnimationFrameResized(fSkillIcons[spbBomber], EXPLOSION, 0, Rect(-2, 7, 15, 24));
+    DrawAnimationFrameResized(fSkillIcons[spbBomber], EXPLOSION, 0, Rect(-2, 7, 15, 22));
 
     // Freezer is tricky - the goal is an outlined frozen lemming over an ice cube
-    DrawAnimationFrame(fSkillIcons[spbFreezer], ICECUBE, 0, 8, 21);
-    DrawAnimationFrame(fSkillIcons[spbFreezer], FROZEN, 0, 7, 22);
-    Outline(fSkillIcons[spbFreezer]);
-    //TempBmp.Assign(fSkillIcons[spbFreezer]);
-    //fSkillIcons[spbFreezer].Clear(0);
+    DrawAnimationFrame(fSkillIcons[spbFreezer], ICECUBE, 0, 8, 20);
+    DrawAnimationFrame(fSkillIcons[spbFreezer], FROZEN, 0, 7, 21);
+    TempBmp.Assign(fSkillIcons[spbFreezer]);
+    fSkillIcons[spbFreezer].Clear(0);
     TempBmp.DrawTo(fSkillIcons[spbFreezer], 0, 0);
 
     // Blocker is simple
-    DrawAnimationFrame(fSkillIcons[spbBlocker], BLOCKING, 4, 7, 21);
+    DrawAnimationFrame(fSkillIcons[spbBlocker], BLOCKING, 4, 7, 20);
 
     // Platformer, Builder and Stacker have bricks drawn to clarify the direction of building.
     // Platformer additionally has some extra black pixels drawn in to make the outline nicer.
-    DrawAnimationFrame(fSkillIcons[spbPlatformer], PLATFORMING, 1, 7, 20);
-    fSkillIcons[spbPlatformer].FillRect(2 * ResMod, 21 * ResMod, 12 * ResMod, 22 * ResMod, $FF000000);
-    DrawBrick(fSkillIcons[spbPlatformer], 2, 21);
-    DrawBrick(fSkillIcons[spbPlatformer], 5, 21);
-    DrawBrick(fSkillIcons[spbPlatformer], 8, 21);
-    DrawBrick(fSkillIcons[spbPlatformer], 11, 21);
+    DrawAnimationFrame(fSkillIcons[spbPlatformer], PLATFORMING, 1, 7, 19);
+    fSkillIcons[spbPlatformer].FillRect(2 * ResMod, 21 * ResMod, 12 * ResMod, 21 * ResMod, $FF000000);
+    DrawBrick(fSkillIcons[spbPlatformer], 2, 20);
+    DrawBrick(fSkillIcons[spbPlatformer], 5, 20);
+    DrawBrick(fSkillIcons[spbPlatformer], 8, 20);
+    DrawBrick(fSkillIcons[spbPlatformer], 11, 20);
 
-    DrawAnimationFrame(fSkillIcons[spbBuilder], BRICKLAYING, 1, 7, 20);
-    DrawBrick(fSkillIcons[spbBuilder], 4, 22);
-    DrawBrick(fSkillIcons[spbBuilder], 6, 21);
-    DrawBrick(fSkillIcons[spbBuilder], 8, 20);
-    DrawBrick(fSkillIcons[spbBuilder], 10, 19);
+    DrawAnimationFrame(fSkillIcons[spbBuilder], BRICKLAYING, 1, 7, 19);
+    DrawBrick(fSkillIcons[spbBuilder], 4, 21);
+    DrawBrick(fSkillIcons[spbBuilder], 6, 20);
+    DrawBrick(fSkillIcons[spbBuilder], 8, 19);
+    DrawBrick(fSkillIcons[spbBuilder], 10, 18);
 
-    DrawAnimationFrame(fSkillIcons[spbStacker], STACKING, 0, 7, 21);
-    DrawBrick(fSkillIcons[spbStacker], 10, 20);
+    DrawAnimationFrame(fSkillIcons[spbStacker], STACKING, 0, 7, 20);
     DrawBrick(fSkillIcons[spbStacker], 10, 19);
     DrawBrick(fSkillIcons[spbStacker], 10, 18);
     DrawBrick(fSkillIcons[spbStacker], 10, 17);
+    DrawBrick(fSkillIcons[spbStacker], 10, 16);
 
     // Projectiles are messy.
     if GameParams.HighResolution then
@@ -761,19 +760,19 @@ begin
 
     DoProjectileRecolor(TempBMP, BrickColor);
 
-    DrawMiscBmp(TempBMP, fSkillIcons[spbSpearer], 2, 8, PROJECTILE_GRAPHIC_RECTS[pgSpearSlightBLTR]);
-    DrawMiscBmp(TempBMP, fSkillIcons[spbGrenader], 10, 8, PROJECTILE_GRAPHIC_RECTS[pgGrenade]);
+    DrawMiscBmp(TempBMP, fSkillIcons[spbSpearer], 2, 7, PROJECTILE_GRAPHIC_RECTS[pgSpearSlightBLTR]);
+    DrawMiscBmp(TempBMP, fSkillIcons[spbGrenader], 10, 7, PROJECTILE_GRAPHIC_RECTS[pgGrenade]);
 
-    DrawAnimationFrame(fSkillIcons[spbSpearer], THROWING, 2, 6, 21);
-    DrawAnimationFrame(fSkillIcons[spbGrenader], THROWING, 3, 3, 21);
+    DrawAnimationFrame(fSkillIcons[spbSpearer], THROWING, 2, 6, 20);
+    DrawAnimationFrame(fSkillIcons[spbGrenader], THROWING, 3, 3, 20);
 
     // Laserer, Basher, Fencer, Miner are all simple - we do have to take care to avoid frames with destruction particles
     // For Digger, we just have to accept some particles.
-    DrawAnimationFrame(fSkillIcons[spbLaserer], LASERING, 0, 8, 21);
-    DrawAnimationFrame(fSkillIcons[spbBasher], BASHING, 0, 8, 21);
-    DrawAnimationFrame(fSkillIcons[spbFencer], FENCING, 1, 7, 21);
-    DrawAnimationFrame(fSkillIcons[spbMiner], MINING, 12, 4, 21);
-    DrawAnimationFrame(fSkillIcons[spbDigger], DIGGING, 4, 7, 21);
+    DrawAnimationFrame(fSkillIcons[spbLaserer], LASERING, 0, 8, 20);
+    DrawAnimationFrame(fSkillIcons[spbBasher], BASHING, 0, 8, 20);
+    DrawAnimationFrame(fSkillIcons[spbFencer], FENCING, 1, 7, 20);
+    DrawAnimationFrame(fSkillIcons[spbMiner], MINING, 12, 4, 20);
+    DrawAnimationFrame(fSkillIcons[spbDigger], DIGGING, 4, 7, 20);
 
     // And finally, outline everything. We generate the cloner after this, as it makes use of
     // the post-outlined Walker graphic.
@@ -783,7 +782,7 @@ begin
         // Swimmer and Cloner are already outlined during their generation.
 
     // Cloner is drawn as two back-to-back walkers, individually outlined.
-    DrawAnimationFrame(fSkillIcons[spbCloner], WALKING_RTL, 1, 6, 21);
+    DrawAnimationFrame(fSkillIcons[spbCloner], WALKING_RTL, 1, 6, 20);
     Outline(fSkillIcons[spbCloner]);
     TempBmp.Assign(fSkillIcons[spbWalker]);
     TempBmp.DrawTo(fSkillIcons[spbCloner], 2, 0); // We want it drawn 2px to the right of where it is in the walker icon
