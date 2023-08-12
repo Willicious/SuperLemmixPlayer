150399427ec1d690ab7c216eb75d49036909964b
Willicious <will3music@gmail.com>
2023-08-13 00:55:53 +0100

    Bugfix - Custom menu music is now loaded when specified by a level pack

--- GameSound.pas
+++ GameSound.pas
@@ -446,15 +446,14 @@ begin
 
   aName := 'menu';
   aPath := GameParams.CurrentLevel.Group.ParentBasePack.Path;
+  GetExtension;
 
-  if FileExists(GameParams.CurrentLevel.Group.FindFile(aName + Ext)) then
+  if FileExists(GameParams.CurrentLevel.Group.Parent.FindFile(aName + Ext)) then
   begin
     fLookingForCustomMenuMusicExt := True;
-    GetExtension;
-    F := TFileStream.Create(aPath + aName + Ext, fmOpenRead)
+    F := TFileStream.Create(aPath + aName + Ext, fmOpenRead);
   end else begin
     fLookingForCustomMenuMusicExt := False;
-    GetExtension;
     F := TFileStream.Create(AppPath + SFMusic + aName + Ext, fmOpenRead);
   end;
   try
