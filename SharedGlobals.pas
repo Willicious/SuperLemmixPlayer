ca998b5bc80b98eee0a0a444c3a4e51cf498d0b4
Willicious <will3music@gmail.com>
2024-06-05 17:41:36 +0100

    About time I did this :P

--- SharedGlobals.pas
+++ SharedGlobals.pas
@@ -5,16 +5,50 @@ unit SharedGlobals;
 interface
 
 uses
-  {$ifdef logging}Dialogs,{$endif} Classes, SysUtils;
+  {$ifdef logging}Dialogs,{$endif} Classes, SysUtils, Windows;
 
 {$ifdef logging}
 var
   DebugLog: TStringList;{$endif}
 
+  // For writing to text log
   procedure DebugMsg(const aString: String);
   procedure Log(const aString: String);
 
+  // For writing to Messages output
+  procedure OutputMarker(aString: String);
+  procedure Output(aString: String) overload;
+  procedure Output(aString: String; i: Integer) overload;
+  procedure Output(aString: String; aBool: Boolean) overload;
+
 implementation
+
+procedure Output(aString: String);
+begin
+  OutputDebugString(PChar(aString));
+end;
+
+procedure Output(aString: String; i: Integer);
+begin
+  OutputDebugString(PChar(aString + ' = ' + i.ToString));
+end;
+
+procedure Output(aString: String; aBool: Boolean);
+var
+  S: String;
+begin
+  if (aBool = True) then S := 'True' else S := 'False';
+  OutputDebugString(PChar(aString + ' = ' + S));
+end;
+
+procedure OutputMarker(aString: String);
+begin
+  if aString = '' then aString := 'Marker';
+
+  OutputDebugString(PChar('////////////////////////////////////////////////////////////////////'));
+  OutputDebugString(PChar('///////////////////////////' + aString + '//////////////////////////'));
+  OutputDebugString(PChar('////////////////////////////////////////////////////////////////////'));
+end;
 
 procedure Log(const aString: String);
 begin
