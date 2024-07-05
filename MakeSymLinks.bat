e7dc083ba47422e92dca4d59ef55ffd255c3b8ba
Willicious <will3music@gmail.com>
2024-07-05 19:31:56 +0100

    MakeSymLinks now links to DMA Compilation and Lemminas Origins (if the latter exists in the external levels directory)

--- MakeSymLinks.bat
+++ MakeSymLinks.bat
@@ -10,3 +10,7 @@ mklink /H bin\NLPackerDefaultData.ini data\external\NLPackerDefaultData.ini
 
 if not exist "bin\levels" mkdir "bin\levels"
 mklink /J "bin\levels\SuperLemmix Welcome Pack" "data\external\levels\SuperLemmix Welcome Pack"
+mklink /J "bin\levels\DMA Lemmings Compilation" "data\external\levels\DMA Lemmings Compilation"
+
+if exist "data\external\levels\Lemminas Origins" (
+mklink /J "bin\levels\Lemminas Origins" "data\external\levels\Lemminas Origins")
