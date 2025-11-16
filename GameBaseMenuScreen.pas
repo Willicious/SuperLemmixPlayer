e12efa08a2691da05df4d83fd743ed9861ea57d1
Willicious <williciousmedia@gmail.com>
2025-11-16 08:30:58 +0000

    Removed some debug outputs

--- GameBaseMenuScreen.pas
+++ GameBaseMenuScreen.pas
@@ -281,14 +281,10 @@ begin
                      and not GameParams.FullScreen;
 
   if UseLargerWidth then
-  begin
-    Output('Using larger width, window width', WindowWidth);
     INTERNAL_SCREEN_WIDTH := 1092
-  end else if UseMediumWidth then
-  begin
-    Output('Using medium width, window width', WindowWidth);
+  else if UseMediumWidth then
     INTERNAL_SCREEN_WIDTH := 990
-  end else
+  else
     INTERNAL_SCREEN_WIDTH := 888;
 
   INTERNAL_SCREEN_HEIGHT := 492;
